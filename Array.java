9be502f304445381e40a57ecd87e001091d7b173
Binita rijal <rijalbinita01@gmail.com>
2024-09-05 22:15:48 +0545

    new class and object

--- Array.java
+++ Array.java
@@ -1,5 +1,8 @@
 public class Array{
+   
     public static void main(String[] args) {
         System.out.println("Hello world");
+       
+        
     }
 }
---
+++ Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    int roll;
+    String name;
+
+    public static void main(String[] args) {
+        Student sita=new Student();
+        sita.roll=34;
+        sita.name="sita adk";
+    }
+    
+}
