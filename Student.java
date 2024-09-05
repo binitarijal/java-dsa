9457e0d7abb0de3802eaccc7fd5d7f15a0ea3ea6
Binita rijal <rijalbinita01@gmail.com>
2024-09-05 22:18:34 +0545

    percentage

--- Student.java
+++ Student.java
@@ -1,11 +1,13 @@
 public class Student {
     int roll;
     String name;
+    float percent= 89.5f;
 
     public static void main(String[] args) {
         Student sita=new Student();
         sita.roll=34;
         sita.name="sita adk";
+System.out.println(sita.percent);
     }
     
 }
