4075bdb3605cb2b5f4b7e856ab7b9b1a56ce43d5
Binita rijal <rijalbinita01@gmail.com>
2024-09-10 06:55:29 +0545

    new oops code

--- Oops.java
+++ Oops.java
@@ -1,7 +1,25 @@
+ class Student{
+    int rollno;
+    String name;
+    static String clzname="ssmc";
+
+    Student(int r, String n){
+        this.rollno=r;
+        this.name=n;
+    }
+    void display(){
+        System.out.println(rollno +" "+name+" "+clzname);
+    }
+ }
+
+
 public class Oops {
     // 
     
     final public static void main(String...args){
-System.out.println("hello world");
+Student s1=new Student(2, "Binita rijal");
+Student s2=new Student(3, "kriti");
+s1.display();
+s2.display();
     }
 }
