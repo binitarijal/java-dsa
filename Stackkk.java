7f814ac24891416ef59a4be354eae83d278b8eee
Binita rijal <rijalbinita01@gmail.com>
2024-09-13 21:56:07 +0545

    using function

--- Stackkk.java
+++ Stackkk.java
@@ -23,31 +23,50 @@
 // }
 
 // reverse stack
-public class Stackkk{
-   public static void main(String[] args) {
-      Stack<Integer> st= new Stack<>();
-         st.push(1);
-         st.push(2);
-         st.push(3);
-         st.push(4);
-         st.push(5);
-         System.out.println(st);
-         Stack<Integer> str=new Stack<>();
-         while(st.size()>0){
-            str.push(st.pop());
-         }
-         System.out.println(str); 
+// public class Stackkk{
+//    public static void main(String[] args) {
+//       Stack<Integer> st= new Stack<>();
+//          st.push(1);
+//          st.push(2);
+//          st.push(3);
+//          st.push(4);
+//          st.push(5);
+//          System.out.println(st);
+//          Stack<Integer> str=new Stack<>();
+//          while(st.size()>0){
+//             str.push(st.pop());
+//          }
+//          System.out.println(str); 
 
-         //copy
-         Stack<Integer> bb=new Stack<>();
-         while(str.size()>0){
-            bb.push(str.pop());
-         }
-         System.out.println(bb);
+//          //copy
+//          Stack<Integer> bb=new Stack<>();
+//          while(str.size()>0){
+//             bb.push(str.pop());
+//          }
+//          System.out.println(bb );
 
 
 
    
 
+//    }
+// }
+
+public class Stackkk{
+   public static void displays(Stack<Integer> st){
+st.push(99);
+st.push(9);
+
+   }
+   public static void main(String[] args) {
+      Stack<Integer> st=new Stack<>();
+      st.push(12);
+      st.push(2);
+      st.push(1);
+      st.push(2);
+      st.push(11);
+      System.out.println(st);
+      displays(st);
+      System.out.println(st);
    }
 }
